50a68091a44eb1b583b1e0923de2d560ec3e71a1
Hiran Ferreti Baccos <hiranjc1@outlook.com>
2025-09-18 18:51:44 -0300

    first commit

---
+++ Entities/BusinessAccount.cs
@@ -0,0 +1,28 @@
+﻿namespace Inheritance4ClassesAbstratas.Entities
+{
+    sealed internal class BusinessAccount : Account
+    {
+        public double LoanLimit { get; set; }
+
+        public BusinessAccount() { }
+
+        public BusinessAccount(int number, string holder, double balance, double loanLimit) : base(number, holder, balance)
+        {
+            LoanLimit = loanLimit;
+        }
+
+        public void Loan(double amount)
+        {
+            if (amount <= LoanLimit)
+            {
+                Balance += amount;
+            }
+        }
+
+        public sealed override void Withdraw(double amount)
+        {
+            base.Withdraw(amount);
+            Balance -= 2.0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Globalization;
+
+using Inheritance4ClassesAbstratas.Entities;
+
+namespace Inheritance4ClassesAbstratas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Account> list = new List<Account>();
+            list.Add(new SavingsAccount(1001, "Alex", 500.00, 0.1));
+            list.Add(new BusinessAccount(1002, "Maria", 500.00, 400.00));
+            list.Add(new SavingsAccount(1003, "Bob", 500.00, 0.1));
+            list.Add(new BusinessAccount(1004, "Anna", 500.00, 500.00));
+
+            double sum = 0.0;
+            foreach (Account acc in list)
+            {
+                sum += acc.Balance;
+            }
+            Console.WriteLine("Total balance: " + sum.ToString("F2", CultureInfo.InvariantCulture));
+
+            foreach (Account acc in list)
+            {
+                acc.Withdraw(10.0);
+            }
+
+            foreach (Account acc in list)
+            {
+                Console.WriteLine("Updated balance for account: "
+                    + acc.Number
+                    + ": "
+                    + acc.Balance.ToString("F2", CultureInfo.InvariantCulture)
+                    );
+            }
+        }
+    }
+}
